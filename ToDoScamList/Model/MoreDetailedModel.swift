68b3f85c58d0133eb3eb31b64c76d372c13a7a0f
dezhest <dezhest@yandex.ru>
2023-02-14 13:08:48 +0400

    Main CoreData Fixed and MD started

---
+++ ToDoScamList/Model/MoreDetailedModel.swift
@@ -0,0 +1,8 @@
+//
+//  MoreDetailedModel.swift
+//  ToDoScamList
+//
+//  Created by Denis Zhesterev on 14.02.2023.
+//
+
+import Foundation
---
+++ ToDoScamList/ViewModel/MoreDetailedViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  MoreDetailedViewModel.swift
+//  ToDoScamList
+//
+//  Created by Denis Zhesterev on 14.02.2023.
+//
+
+import Foundation
